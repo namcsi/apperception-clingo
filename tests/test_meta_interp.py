bde0c487c6c454c6eb0a72232c24478c88423ffa
Amade Nemes <nemesamade@gmail.com>
2024-12-15 00:19:54 +0100

    Implement standard metainterpreter.

--- tests/test_meta_interp.py
+++ tests/test_meta_interp.py
@@ -29,33 +29,40 @@ class TestMeta(TestCase):
 
     def assertModelEqual(
         self,
-        input_files: List[Path],
+        meta_interpreters: List[Path],
+        input_file: Path,
         result: List[str] | False,
         clingo_args: List[str] = clingo_args,
     ) -> None:
         """
         Assert that a model's set of atoms equal a set of atoms, or unsat.
         """
-        ctl = Control(clingo_args)
-        for f in input_files:
-            ctl.load(str(f))
-        ctl.ground()
-        with ctl.solve(yield_=True) as handle:
-            if result is not False:
-                expected = set(parse_term(s) for s in result)
-                for model in handle:
-                    found = set(model.symbols(shown=True))
-                    self.assertSetEqual(found, expected)
-                self.assertTrue(handle.get().satisfiable)
-            else:
-                self.assertTrue(handle.get().unsatisfiable)
+        for interpreter in meta_interpreters:
+            ctl = Control(clingo_args)
+            ctl.load(str(interpreter))
+            ctl.load(str(input_file))
+            ctl.add("#defined exist/1.")
+            ctl.ground()
+            with ctl.solve(yield_=True) as handle:
+                if result is not False:
+                    expected = set(parse_term(s) for s in result)
+                    for model in handle:
+                        found = set(model.symbols(shown=True))
+                        self.assertSetEqual(found, expected)
+                    self.assertTrue(handle.get().satisfiable)
+                else:
+                    self.assertTrue(handle.get().unsatisfiable)
 
-    def test_tight_on_static_tight(self) -> None:
+    def test_static_tight(self) -> None:
         """
         Test the tight interpreter on tight static program.
         """
         self.assertModelEqual(
-            [encoding_dir / "meta-tight.lp", meta_test_dir / "static-tight.lp"],
+            [
+                encoding_dir / "body-decoupled" / "meta-tight.lp",
+                encoding_dir / "standard" / "meta.lp",
+            ],
+            meta_test_dir / "static-tight.lp",
             [
                 "hold(s(pred(p,1),obj(a)),1)",
                 "hold(s(pred(q,1),obj(a)),1)",
@@ -67,14 +74,20 @@ class TestMeta(TestCase):
         """
         Test the tight interpreter on a non-tight static program.
         """
-        self.assertModelEqual([encoding_dir / "meta-tight.lp", meta_test_dir / "static-loop-found.lp"], False)
+        self.assertModelEqual(
+            [encoding_dir / "body-decoupled" / "meta-tight.lp"], meta_test_dir / "static-loop-found.lp", False
+        )
 
     def test_nontight_static_loop_founded(self) -> None:
         """
         Test the nontight interpreter on non-tight static program with a founded loop.
         """
         self.assertModelEqual(
-            [encoding_dir / "meta-nontight.lp", meta_test_dir / "static-loop-found.lp"],
+            [
+                encoding_dir / "body-decoupled" / "meta-nontight.lp",
+                encoding_dir / "standard" / "meta.lp",
+            ],
+            meta_test_dir / "static-loop-found.lp",
             ["hold(s(pred(p,1),obj(a)),1)", "hold(s(pred(q,1),obj(a)),1)"],
         )
 
@@ -83,7 +96,11 @@ class TestMeta(TestCase):
         Test the nontight interpreter on non-tight static program with an unfounded loop.
         """
         self.assertModelEqual(
-            [encoding_dir / "meta-nontight.lp", meta_test_dir / "static-loop-unfound.lp"],
+            [
+                encoding_dir / "body-decoupled" / "meta-nontight.lp",
+                encoding_dir / "standard" / "meta.lp",
+            ],
+            meta_test_dir / "static-loop-unfound.lp",
             False,
         )
 
@@ -92,7 +109,11 @@ class TestMeta(TestCase):
         Test the interpreter on a simple causal program.
         """
         self.assertModelEqual(
-            [encoding_dir / "meta-nontight.lp", meta_test_dir / "causal.lp"],
+            [
+                encoding_dir / "body-decoupled" / "meta-nontight.lp",
+                encoding_dir / "standard" / "meta.lp",
+            ],
+            meta_test_dir / "causal.lp",
             [
                 "hold(s(pred(p,1),obj(a)),1)",
                 "hold(s(pred(p',1),obj(a)),2)",
@@ -105,7 +126,11 @@ class TestMeta(TestCase):
         Test the interpreter on a program with causal rules, inertia and incompossibility.
         """
         self.assertModelEqual(
-            [encoding_dir / "meta-tight.lp", meta_test_dir / "incompos-xor.lp"],
+            [
+                encoding_dir / "body-decoupled" / "meta-tight.lp",
+                encoding_dir / "standard" / "meta.lp",
+            ],
+            meta_test_dir / "incompos-xor.lp",
             [
                 "hold(s(pred(f,1),obj(a)),1)",
                 "hold(s(pred(p,1),obj(a)),1)",
@@ -121,7 +146,11 @@ class TestMeta(TestCase):
         Test the interpreter on a program with causal rules, inertia and incompossibility.
         """
         self.assertModelEqual(
-            [encoding_dir / "meta-tight.lp", meta_test_dir / "incompos-exist.lp"],
+            [
+                encoding_dir / "body-decoupled" / "meta-tight.lp",
+                encoding_dir / "standard" / "meta.lp",
+            ],
+            meta_test_dir / "incompos-exist.lp",
             [
                 "hold(s2(pred(q,2),obj(b),obj(c)),1)",
                 "hold(s2(pred(q,2),obj(c),obj(b)),1)",
@@ -151,7 +180,11 @@ class TestMeta(TestCase):
         Test the interpreter on a unified interpretation example 6 given in the paper.
         """
         self.assertModelEqual(
-            [encoding_dir / "meta-tight.lp", meta_test_dir / "paper-example6.lp"],
+            [
+                encoding_dir / "body-decoupled" / "meta-tight.lp",
+                encoding_dir / "standard" / "meta.lp",
+            ],
+            meta_test_dir / "paper-example6.lp",
             [
                 "hold(s2(pred(r,2),obj(a),obj(b)),1)",
                 "hold(s2(pred(r,2),obj(b),obj(a)),1)",
@@ -221,7 +254,11 @@ class TestMeta(TestCase):
         Test the interpreter on a unified interpretation example 7 given in the paper.
         """
         self.assertModelEqual(
-            [encoding_dir / "meta-tight.lp", meta_test_dir / "paper-example7.lp"],
+            [
+                encoding_dir / "body-decoupled" / "meta-tight.lp",
+                encoding_dir / "standard" / "meta.lp",
+            ],
+            meta_test_dir / "paper-example7.lp",
             [
                 "hold(s2(pred(r,2),obj(a),obj(b)),1)",
                 "hold(s2(pred(r,2),obj(b),obj(c)),1)",
